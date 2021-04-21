1cb9c750a64a4daed0fbdc5a887adb6a707abf88
Aiden <wilsonvideo@outlook.com>
2021-04-22 00:57:16 +1000

    External Data example Project.

---
+++ ExampleUnrealProject/Plugins/ExternalDataPlugin/Source/ExternalData/Public/ExternalDataBPLibrary.h
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Containers/UnrealString.h"
+#include "Containers/Array.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Misc/Char.h"
+#include "ExternalDataBPLibrary.generated.h"
+
+
+/* 
+*	Function library class.
+*	Each function in it is expected to be static and represents blueprint node that can be called in any blueprint.
+*
+*	When declaring function you can define metadata for the node. Key function specifiers will be BlueprintPure and BlueprintCallable.
+*	BlueprintPure - means the function does not affect the owning object in any way and thus creates a node without Exec pins.
+*	BlueprintCallable - makes a function which can be executed in Blueprints - Thus it has Exec pins.
+*	DisplayName - full name of the node, shown when you mouse over the node and in the blueprint drop down menu.
+*				Its lets you name the node using characters not allowed in C++ function names.
+*	CompactNodeTitle - the word(s) that appear on the node.
+*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu. 
+*				Good example is "Print String" node which you can find also by using keyword "log".
+*	Category -	the category your node will be under in the Blueprint drop-down menu.
+*
+*	For more info on custom blueprint nodes visit documentation:
+*	https://wiki.unrealengine.com/Custom_Blueprint_Node_Creation
+*/
+UCLASS()
+class UExternalDataBPLibrary : public UBlueprintFunctionLibrary
+{
+
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "External Data", meta = (Keywords = "Load Float Data"))
+		static TArray<float> LoadFloatData(FString FileName);
+
+	UFUNCTION(BlueprintCallable, Category = "External Data", meta = (Keywords = "Save Float Data"))
+		static bool SaveFloatData(FString FileName, TArray<float> SaveFloat);
+
+};
